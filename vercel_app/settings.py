85a5f816abb47bdfc92265d58be07f846324ef92
albabernal03 <ALBABERO@MYUAX.COM>
2023-12-14 14:21:54 +0100

    c

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders', # Django module for handling CORS
-    'example'
+    'example',
     'rest_framework',
     'djoser',
     'cuentas',
