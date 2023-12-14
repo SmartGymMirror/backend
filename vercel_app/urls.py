d036a321186a650a71df5327914f9d29a94c0eb0
albabernal03 <ALBABERO@MYUAX.COM>
2023-12-14 15:14:20 +0100

    c

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -6,7 +6,4 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('example.urls')),
-    path('auth/', include('djoser.urls')),  # URLs generales de autenticación
-    path('auth/', include('djoser.urls.jwt')),  # URLs JWT
-    path('', TemplateView.as_view(template_name='index.html')),  # Ruta específica para el TemplateView
 ]
