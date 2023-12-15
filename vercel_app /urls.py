d9db02100c0c8a2ae3c6250e8962924f731ecdf3
albabernal03 <ALBABERO@MYUAX.COM>
2023-12-15 22:56:22 +0100

    subo

--- vercel_app /urls.py
+++ vercel_app /urls.py
@@ -6,5 +6,4 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('example.urls')),
-    path('', include('autentificacion.urls')),
 ]
