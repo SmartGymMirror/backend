683b323b861ff05ae59a3692a508da44bfa4a666
albabernal03 <ALBABERO@MYUAX.COM>
2023-12-15 22:41:16 +0100

    borro eso que no funcionaba

--- autentificacion/apps.py
+++ autentificacion/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CuentasConfig(AppConfig):
+class AutentificacionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "cuentas"
+    name = "autentificacion"
--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'cuentas',
-    'mediapipe',
+    'rest_framework_simplejwt',
+    'autentificacion',
 ]
 
 MIDDLEWARE = [
